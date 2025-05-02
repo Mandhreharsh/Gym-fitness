1ed381a24f2acf40095587155c225e522217696d
Harsh Mandhre <mandhreharsh@gmail.com>
2025-05-02 20:51:04 +0530

    Update CORS configuration to support dynamic origins

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -37,7 +37,7 @@ const Loginform = ({ setIsLoggedIn }) => {
         }
 
         try {
-            const response = await axios.post("https://your-backend-name.onrender.com/api/v1/user/login", {
+            const response = await axios.post("https://gym-fitness-ohjb.onrender.com/api/v1/user/login", {
                 email: formData.email,
                 password: formData.password
             });
