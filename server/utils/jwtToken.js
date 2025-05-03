d07457df1717f68e80fb74bcceb967b2354b5613
Harsh Mandhre <mandhreharsh@gmail.com>
2025-05-03 13:23:15 +0530

    update

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,16 +1,15 @@
 export const generateToken = (user, message, statusCode, res) => {
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
-  res.status(statusCode)
-    .cookie("patientToken", token, {
-      httpOnly: true,
-      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-      sameSite: "None", // for cross-site cookies (Vercel + Render)
-      secure: true,     // only on https
-    })
-    .json({
-      success: true,
-      message,
-      user,
-    });
-};
+    const token = user.generateJsonWebToken();
+    
+    res.status(statusCode)
+      .cookie("patientToken", token, {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 1000),
+        httpOnly: true,
+      })
+      .json({
+        success: true,
+        message,
+        user,
+        token,
+      });
+  };
